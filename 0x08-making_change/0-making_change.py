1322678381fb240dbe3694ace082cd4dccc00288
elbatouri rabi <elbatouri@gmail.com>
2024-05-02 00:24:20 +0000

    correction code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
+"""
+Chamge comes from within
+"""
+
 
 def makeChange(coins, total):
     """
-    Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet a given amount total.
+    Given a pile of cons of different values,
+    determine the fewest number of cons needed to meet a given amount total.
     """
     if total <= 0:
         return 0
-    
-    # Initialize the dynamic programming array
-    dp = [0] + [float('inf')] * total
-    
-    # Iterate through each coin denomination
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    # Check if the total can be achieved
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[-1] if dp[-1] != float("inf") else -1
