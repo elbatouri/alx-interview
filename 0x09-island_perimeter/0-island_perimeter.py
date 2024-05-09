6277603656426a998899bb534a18b4df8361b136
elbatouri rabi <elbatouri@gmail.Com>
2024-05-09 14:38:35 -0700

    change my grid to grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,16 +4,16 @@ Returns the perimeter of the island described in the grid.
 :param my_grid: 2D list representing the land and water arrangement.
 :return: The perimeter of the island.
 """
-def island_perimeter(my_grid):
+def island_perimeter(grid):
     perimeter = 0
-    rows = len(my_grid)
-    cols = len(my_grid[0])
+    rows = len(grid)
+    cols = len(grid[0])
     for i in range(rows):
         for j in range(cols):
-            if my_grid[i][j] == 1:
+            if grid[i][j] == 1:
                 perimeter += 4
-                if i > 0 and my_grid[i - 1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if j > 0 and my_grid[i][j - 1] == 1:
+                if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
     return perimeter
